e44ab389c1939cb7060e88a8d64861a79917daa9
naoki kodama <naoki.koda@gmail.com>
2016-09-18 00:44:27 +0900

    * .gitignore: テストプロジェクトを追加。テストケースを追加。処理は空。
    * crontab.sln:
    * Crontab.cs:
    * Test.cs:
    * crontab.csproj:
    * packages.config:
    * crontab_test.csproj:
    * AssemblyInfo.cs:
    * ChainingAssertion.NUnit.cs:

---
+++ crontab/Crontab.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace crontab
+{
+	public class Crontab
+	{
+		public Crontab(string source)
+		{
+		}
+
+		public DateTime GetNext(DateTime baseTime)
+		{
+			return new DateTime();
+		}
+	}
+}
---
+++ crontab_test/Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using crontab;
+
+namespace crontab_test
+{
+	[TestFixture()]
+	public class Test
+	{
+		[Test()]
+		public void 秒指定の次の時間が取得できること()
+		{
+			// 秒、分、時、日、月、曜日
+			var target = new Crontab("10 * * * * *");
+			var nextTime = target.GetNext(new DateTime(2016, 9, 18, 0, 25, 0));
+			nextTime.Is(new DateTime(2016, 9, 18, 0, 25, 10));
+		}
+	}
+}
